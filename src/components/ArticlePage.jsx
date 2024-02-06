fb4fa4916bc8bcca22a36bf729c4db8d08f33478
AudeNgD <aude.nguyenduc@gmail.com>
2024-02-06 11:41:05 +0000

    add new comment feature on article page

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -6,10 +6,12 @@ import {
   patchArticleVotes,
 } from "../../utils/api";
 import CommentsList from "./CommentsList";
+import NewCommentForm from "./NewCommentForm";
 
 export default function ArticlePage() {
   const { articleId } = useParams();
   const [error, setError] = useState(null);
+  const [toggle, isToggled] = useState(false);
 
   const [singleArticle, setSingleArticle] = useState([]);
   const [comments, setComments] = useState([]);
@@ -60,20 +62,48 @@ export default function ArticlePage() {
       });
   }
 
+  function handleCommentClick(event) {
+    toggle ? isToggled(false) : isToggled(true);
+  }
+
+  function addNewCommentToList(comment) {
+    fetchComments(articleId).then(() => {
+      comment.votes = 0;
+      setComments((currentComments) => {
+        return [comment, ...currentComments];
+      });
+    });
+  }
+
   return (
     <>
       <img id="article--image" src={article_img_url} />
       <h2>{title}</h2>
       <p>{body}</p>
-      <div className="vote">
-        <button className="button--vote" onClick={handleClickVote} value="1">
-          &#128077;{" "}
-        </button>
-        <button className="button--vote" onClick={handleClickVote} value="-1">
-          &#128078;
-        </button>
-        <p>{votes}</p>
+      <div className="section--interaction">
+        <div className="vote">
+          <button className="button--vote" onClick={handleClickVote} value="1">
+            &#128077;{" "}
+          </button>
+          <button className="button--vote" onClick={handleClickVote} value="-1">
+            &#128078;
+          </button>
+          <p>{votes}</p>
+        </div>
+        <div className="comment-feature">
+          <button className="button--new-comment" onClick={handleCommentClick}>
+            Add a comment
+          </button>
+        </div>
       </div>
+      {toggle ? (
+        <NewCommentForm
+          article_id={article_id}
+          addNewCommentToList={addNewCommentToList}
+          toggle={toggle}
+          isToggled={isToggled}
+        />
+      ) : null}
       <CommentsList comments={comments} />
     </>
   );
