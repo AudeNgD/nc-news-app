2d15c23c61f35ea475fafffaf5f3aa877712991c
AudeNgD <aude.nguyenduc@gmail.com>
2024-02-09 15:49:29 +0000

    complete implementation new article feature

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import CurrentGuestContext from "../contexts/CurrentGuestContext";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Expire from "./Expire";
 
 export default function Sidebar() {
   const { guest } = useContext(CurrentGuestContext);
@@ -11,12 +12,18 @@ export default function Sidebar() {
   function handleClickNewArticle(event) {
     if (guest !== "guest") {
       navigate("/home/articles/new-article");
+    } else {
+      setError("You must be logged in to add a new article");
     }
   }
+
   return (
     <>
       <section id="sidebar">
         <button onClick={handleClickNewArticle}>Post a new article</button>
+        <Expire delay="5000">
+          <p className="error--message">{error}</p>
+        </Expire>
       </section>
     </>
   );
