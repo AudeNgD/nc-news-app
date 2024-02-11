bc5137d0dca072998846e0dcfa401622f3ec214e
AudeNgD <aude.nguyenduc@gmail.com>
2024-02-11 19:37:13 +0000

    fix comment list for guest user

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,17 +1,22 @@
 import { useContext, useState } from "react";
 import CommentItem from "./CommentItem";
 import CurrentUserContext from "../contexts/CurrentUserContext";
+import CurrentGuestContext from "../contexts/CurrentGuestContext";
 
 export default function CommentsList(props) {
   const { comments } = props;
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+  const { guest, setGuest } = useContext(CurrentGuestContext);
 
   return (
     <div id="comment--section">
       <h3>Comments</h3>
       {comments.map((comment) => {
-        if (currentUser !== "" && comment.author === currentUser.username) {
-          comment.author = "You";
+        console.log(currentUser);
+        if (guest === "") {
+          if (comment.author === currentUser.username) {
+            comment.author = "You";
+          }
         }
         return (
           <CommentItem
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ export default function NavBar() {
     <nav>
       <Link to={`/home`}>
         <img
-          src="../src/assets/logo.png"
+          src="https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           id="main--logo"
           alt="nc news logo - a black and white computer drawing"
         ></img>
